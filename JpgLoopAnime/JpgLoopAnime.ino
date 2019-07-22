90ed63ab38c84eda4ea08eff6cdb872189855e86
lovyan03 <42724151+lovyan03@users.noreply.github.com>
2019-07-22 22:25:05 +0900

    Update: support directory change.

--- JpgLoopAnime/JpgLoopAnime.ino
+++ JpgLoopAnime/JpgLoopAnime.ino
@@ -10,12 +10,35 @@
 MainClass main;
 
 // ここで画像ファイルのディレクトリ名を指定する
-static const char imageDir[] = {"/image_dirname"};
-
+static std::vector<String> imageDirs = {"/image_dirname1", "/image_dirname2"};
+static uint8_t dirIndex = 0;
 
 std::vector<uint8_t*> fbuf;
 std::vector<int32_t> fbufsize;
 uint32_t fpsCount = 0, fpsSec = 0;
+
+bool loadImages(const String& path)
+{
+  bool res = false;
+  for (int i = 0; i < fbuf.size(); i++) free(fbuf[i]);
+  fbuf.clear();
+  fbufsize.clear();
+  Serial.println(path);
+  File root = SD.open(path);
+  File file = root.openNextFile();
+  uint8_t* tmp;
+  while (file) {
+    tmp = (uint8_t*)pvPortMallocCaps(file.size(), MALLOC_CAP_DEFAULT);
+    if (tmp > 0) {
+      file.read(tmp, file.size());
+      fbufsize.push_back(file.size());
+      fbuf.push_back(tmp);
+      res = true;
+    }
+    file = root.openNextFile();
+  }
+  return res;
+}
 
 void setup() {
 
@@ -39,20 +62,21 @@ void setup() {
             , TFT_DC
             );
 
-  Serial.println(imageDir);
-  File root = SD.open(imageDir);
-  File file = root.openNextFile();
-  while (file) {
-    fbufsize.push_back(file.size());
-    fbuf.push_back((uint8_t*)pvPortMallocCaps(fbufsize.back(), MALLOC_CAP_DEFAULT));
-    file.read(fbuf.back(), fbufsize.back());
-    file = root.openNextFile();
-  }
+  loadImages(imageDirs[dirIndex]);
 }
 
 void loop() {
   for (int i = 0; i < fbuf.size(); i++) {
-//      M5.Lcd.drawJpg(fbuf[i], fbufsize[i]);
+    M5.update();
+    if (M5.BtnB.wasPressed() || M5.BtnC.wasPressed()) {
+      delay(100);
+      do {
+        dirIndex = (dirIndex + (M5.BtnC.wasPressed() ? 1 : M5.BtnB.wasPressed() ? imageDirs.size() - 1 : 0)) % imageDirs.size();
+      } while (!loadImages(imageDirs[dirIndex]));
+      M5.Lcd.fillRect(0,0,M5.Lcd.width(),M5.Lcd.height(),0);
+      break;
+    }
+//  M5.Lcd.drawJpg(fbuf[i], fbufsize[i]);
     main.drawJpg(fbuf[i], fbufsize[i]);
     fpsCount++;
     if (fpsSec != millis() / 1000) {
