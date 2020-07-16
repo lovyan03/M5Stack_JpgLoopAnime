6950e39e776f7c404bdd64eba8d9f51645cb1c19
lovyan03 <42724151+lovyan03@users.noreply.github.com>
2020-07-16 17:31:54 +0900

    add support 24bit mode panel

--- JpgLoopAnime/src/MainClass.h
+++ JpgLoopAnime/src/MainClass.h
@@ -21,8 +21,12 @@ public:
     _lcd = lcd;
     _lcd_width = lcd->width();
     _lcd_height = lcd->height();
-
-    for (int i = 0; i < 2; ++i) _dmabufs[i] = (uint16_t*)heap_caps_malloc(_lcd_width * 16 * 2, MALLOC_CAP_DMA);
+    _bytesize = lcd->getColorConverter()->bytes;
+    _fp_jpgWrite = (_bytesize == 2)
+                 ? jpgWrite16
+                 : jpgWrite24
+                 ;
+    for (int i = 0; i < 2; ++i) _dmabufs[i] = (uint8_t*)heap_caps_malloc(_lcd_width * 48 * _bytesize, MALLOC_CAP_DMA);
 
     _dmabuf = _dmabufs[0];
 
@@ -41,9 +45,24 @@ public:
       return false;
     }
 
-    _jpg_x = (_lcd_width - _jdec.width) / 2;
-    _jpg_y = (_lcd_height- _jdec.height) / 2;
-    jres = _jdec.decomp_multitask(jpgWrite, jpgWriteRow);
+    _out_width = std::min<int32_t>(_jdec.width, _lcd_width);
+    _jpg_x = (_lcd_width - _jdec.width) >> 1;
+    if (0 > _jpg_x) {
+      _off_x = - _jpg_x;
+      _jpg_x = 0;
+    } else {
+      _off_x = 0;
+    }
+    _out_height = std::min<int32_t>(_jdec.height, _lcd_height);
+    _jpg_y = (_lcd_height- _jdec.height) >> 1;
+    if (0 > _jpg_y) {
+      _off_y = - _jpg_y;
+      _jpg_y = 0;
+    } else {
+      _off_y = 0;
+    }
+
+    jres = _jdec.decomp_multitask(_fp_jpgWrite, jpgWriteRow);
     if (jres != TJpgD::JDR_OK) {
       Serial.printf("decomp failed! %d\r\n", jres);
       return false;
@@ -53,18 +72,25 @@ public:
 
 private:
   LovyanGFX* _lcd;
-  uint16_t* _dmabufs[2];
-  uint16_t* _dmabuf;
+  uint_fast8_t _bytesize;
+  uint8_t* _dmabufs[2];
+  uint8_t* _dmabuf;
   TJpgD _jdec;
 
   int32_t _remain = 0;
   const uint8_t* _filebuf;
   uint32_t _fileindex;
 
-  uint_fast16_t _lcd_width;
-  uint_fast16_t _lcd_height;
-  uint_fast16_t _jpg_x;
-  uint_fast16_t _jpg_y;
+  int32_t _lcd_width;
+  int32_t _lcd_height;
+  int32_t _out_width;
+  int32_t _out_height;
+  int32_t _off_x;
+  int32_t _off_y;
+  int32_t _jpg_x;
+  int32_t _jpg_y;
+
+  uint32_t(*_fp_jpgWrite)(TJpgD*,void*,TJpgD::JRECT*);
 
   static uint32_t jpgRead(TJpgD *jdec, uint8_t *buf, uint32_t len) {
     MainClass* me = (MainClass*)jdec->device;
@@ -77,21 +103,103 @@ private:
     return len;
   }
 
-  static uint32_t jpgWrite(TJpgD *jdec, void *bitmap, TJpgD::JRECT *rect) {
+  // for 24bit color panel
+  static uint32_t jpgWrite24(TJpgD *jdec, void *bitmap, TJpgD::JRECT *rect) {
     MainClass* me = (MainClass*)jdec->device;
-    uint16_t *p = me->_dmabuf;
-    uint16_t *data = (uint16_t*)bitmap;
-    int_fast16_t width = jdec->width;
-    int_fast16_t x = rect->left;
-    int_fast16_t y = rect->top;
-    int_fast8_t w = rect->right + 1 - x;
-    int_fast8_t h = rect->bottom + 1 - y;
 
-    p += x;
+    auto dst = me->_dmabuf;
+
+    uint_fast16_t x = rect->left;
+    uint_fast16_t y = rect->top;
+    uint_fast16_t w = rect->right + 1 - x;
+    uint_fast16_t h = rect->bottom + 1 - y;
+    uint_fast16_t outWidth = me->_out_width;
+    uint_fast16_t outHeight = me->_out_height;
+    uint8_t *src = (uint8_t*)bitmap;
+    uint_fast16_t oL = 0, oR = 0;
+
+    if (rect->right < me->_off_x)      return 1;
+    if (x >= (me->_off_x + outWidth))  return 1;
+    if (rect->bottom < me->_off_y)     return 1;
+    if (y >= (me->_off_y + outHeight)) return 1;
+
+    int32_t src_idx = 0;
+    int32_t dst_idx = 0;
+
+    if (me->_off_y > y) {
+      uint_fast16_t linesToSkip = me->_off_y - y;
+      src_idx += linesToSkip * w;
+      h -= linesToSkip;
+    }
+
+    if (me->_off_x > x) {
+      oL = me->_off_x - x;
+    }
+    if (rect->right >= (me->_off_x + outWidth)) {
+      oR = (rect->right + 1) - (me->_off_x + outWidth);
+    }
+
+    int_fast16_t line = (w - ( oL + oR )) * 3;
+    dst_idx += oL + x - me->_off_x;
+    src_idx += oL;
     do {
-      memcpy(p, data, w * 2);
-      data += w;
-      p += width;
+      memcpy(&dst[dst_idx * 3], &src[src_idx * 3], line);
+      dst_idx += outWidth;
+      src_idx += w;
+    } while (--h);
+
+    return 1;
+  }
+
+  // for 16bit color panel
+  static uint32_t jpgWrite16(TJpgD *jdec, void *bitmap, TJpgD::JRECT *rect) {
+    MainClass* me = (MainClass*)jdec->device;
+
+    uint16_t *dst = (uint16_t *)me->_dmabuf;
+
+    uint_fast16_t x = rect->left;
+    uint_fast16_t y = rect->top;
+    uint_fast16_t w = rect->right + 1 - x;
+    uint_fast16_t h = rect->bottom + 1 - y;
+    uint_fast16_t outWidth = me->_out_width;
+    uint_fast16_t outHeight = me->_out_height;
+    uint8_t *src = (uint8_t*)bitmap;
+    uint_fast16_t oL = 0, oR = 0;
+
+    if (rect->right < me->_off_x)      return 1;
+    if (x >= (me->_off_x + outWidth))  return 1;
+    if (rect->bottom < me->_off_y)     return 1;
+    if (y >= (me->_off_y + outHeight)) return 1;
+
+    if (me->_off_y > y) {
+      uint_fast16_t linesToSkip = me->_off_y - y;
+      src += linesToSkip * w * 3;
+      h -= linesToSkip;
+    }
+
+    if (me->_off_x > x) {
+      oL = me->_off_x - x;
+    }
+    if (rect->right >= (me->_off_x + outWidth)) {
+      oR = (rect->right + 1) - (me->_off_x + outWidth);
+    }
+
+    int_fast16_t line = (w - ( oL + oR ));
+    dst += oL + x - me->_off_x;
+    src += oL * 3;
+    do {
+      int i = 0;
+      do {
+        uint_fast8_t r8 = src[i*3+0] & 0xF8;
+        uint_fast8_t g8 = src[i*3+1];
+        uint_fast8_t b5 = src[i*3+2] >> 3;
+        r8 |= g8 >> 5;
+        g8 &= 0x1C;
+        b5 = (g8 << 3) + b5;
+        dst[i] = r8 | b5 << 8;
+      } while (++i != line);
+      dst += outWidth;
+      src += w * 3;
     } while (--h);
 
     return 1;
@@ -100,9 +208,9 @@ private:
   static uint32_t jpgWriteRow(TJpgD *jdec, uint32_t y, uint32_t h) {
     static int flip = 0;
     MainClass* me = (MainClass*)jdec->device;
-    if (y == 0)
+    if (y == 0) {
       me->_lcd->setAddrWindow(me->_jpg_x, me->_jpg_y, jdec->width, jdec->height);
-
+    }
     me->_lcd->pushPixelsDMA(me->_dmabuf, jdec->width * h);
 
     flip = !flip;
